53184ac5264bcabe312653577337c5d961ab24bb
ApoptsJoh39 <159179218+ApoptsJoh39@users.noreply.github.com>
2025-11-22 03:18:00 -0600

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,15 +1,13 @@
 @echo off
-echo Descargando libreria JSON...
-mkdir lib 2>nul
-cd lib
-powershell -Command "Invoke-WebRequest -Uri 'https://repo1.maven.org/maven2/org/json/json/20230227/json-20230227.jar' -OutFile 'json.jar'"
-cd ..
-
 echo Compilando proyecto...
-javac -cp "lib/json.jar;." -d bin src/src/com/DAO/*.java src/src/com/server/*.java
 
-echo Compilacion completada!
-echo.
-echo Para ejecutar el servidor, usa:
-echo run.bat
+:: Compilar incluyendo la libreria JSON
+javac -cp "lib/*" -d bin src/src/com/server/BioreactorServlet.java src/src/com/DAO/*.java src/src/com/Aritmetic/*.java src/src/com/MAIN/*.java
+
+if %ERRORLEVEL% EQU 0 (
+    echo Compilacion exitosa!
+) else (
+    echo Error en la compilacion
+)
+
 pause
